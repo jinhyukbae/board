af855eb2da0b4ac59140f0accf39d6a4daafd39c
배진혁 <qowlsgur133@gmail.com>
2023-03-25 18:45:56 +0900

    도메인 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.78.54.28']
+ALLOWED_HOSTS = ['52.78.54.28', 'epicwhale.shop']
 STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
 DEBUG = False
